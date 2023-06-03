536d10678fc43af63076b863fa6a280791639015
DKsolomon <DKSolomon8135@gmail.com>
2023-06-02 17:02:12 -0700

    added routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,7 @@ const userController = {
     }
   },
 
-  async getSingleUser(req, res) {
+  async getUserById(req, res) {
     try {
       const user = await User.findById(req.params.id)
         .populate('thoughts')
@@ -35,18 +35,6 @@ const userController = {
     }
   },
 
-  async deleteUser(req,res) {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-            return res.status(404).json({ message: 'No user with that ID' });
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-},
-
   async updateUser(req,res) {
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, 
@@ -57,6 +45,18 @@ const userController = {
         res.json(user);
     } catch (err) {
         res.status(500).json(err);
+    }
+},
+
+async deleteUser(req,res) {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err)
     }
 },
 
